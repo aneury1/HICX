37595bf09e7145e7ab988b820827b7b4b29e3937
Aneury Perez <aneury1@gmail.com>
2023-03-11 08:15:22 -0400

    Initial commit

---
+++ src/main/java/com/github/aneury1/hicx/App.java
@@ -0,0 +1,13 @@
+package com.github.aneury1.hicx;
+
+/**
+ * This class Represent the Main entry point of the Fi
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
